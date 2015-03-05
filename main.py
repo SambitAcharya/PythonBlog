6911bfd89dc47c0ca081786a14c010d89aa139d6
Sambit Acharya <Sambit@Sambits-MacBook-Air.local>
2015-03-05 12:18:08 +0530

    Experimenting with get and post.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import webapp2
 
 form = """
-    <form action="http://www.google.com/search">
+    <form method="post" action="/testform">
       <input name="q">
       <input type="submit">
     </form>
 """
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello Udacity!')
-        self.response.write(form)
+        # self.response.write('Hello Udacity!')
+        self.response.out.write(form)
 
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+
+class TestHandler(webapp2.RequestHandler):
+    def post(self):
+        # self.response.write('Hello Udacity!')
+        q = self.request.get("q")
+        self.response.out.write(q)
+
+        # self.response.headers['Content-Type'] = 'text/plain'
+        # self.response.write(self.request)
+
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/testform',TestHandler)],
+                               debug=True)
